e7e0705dd8d2657d40a4bc394a397b45b688f60f
aping <czp010443@aliyun.com>
2022-10-13 11:13:03 +0800

    stripping

--- rest/api/market/get_tickers.go
+++ rest/api/market/get_tickers.go
@@ -17,22 +17,5 @@ type GetTickersParam struct {
 
 type GetTickersResponse struct {
 	api.Response
-	Data []struct {
-		InstType  string `json:"instType"`
-		InstId    string `json:"instId"`
-		Last      string `json:"last"`
-		LastSz    string `json:"lastSz"`
-		AskPx     string `json:"askPx"`
-		AskSz     string `json:"askSz"`
-		BidPx     string `json:"bidPx"`
-		BidSz     string `json:"bidSz"`
-		Open24h   string `json:"open24h"`
-		High24h   string `json:"high24h"`
-		Low24h    string `json:"low24h"`
-		VolCcy24h string `json:"volCcy24h"`
-		Vol24h    string `json:"vol24h"`
-		SodUtc0   string `json:"sodUtc0"`
-		SodUtc8   string `json:"sodUtc8"`
-		Ts        int64  `json:"ts,string"`
-	} `json:"data"`
+	Data []Ticker `json:"data"`
 }
