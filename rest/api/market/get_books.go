38de19a371bb762bf254f54c792889585a34f26d
aping <czp010443@aliyun.com>
2022-10-13 12:09:41 +0800

    /api/v5/market/books

---
+++ rest/api/market/get_books.go
@@ -0,0 +1,25 @@
+package market
+
+import "github.com/iaping/go-okx/rest/api"
+
+func NewGetBooks(param *GetBooksParam) (api.IRequest, api.IResponse) {
+	return &api.Request{
+		Path:   "/api/v5/market/books",
+		Method: api.MethodGet,
+		Param:  param,
+	}, &GetBooksResponse{}
+}
+
+type GetBooksParam struct {
+	InstId string `url:"instId"`
+	Sz     string `url:"sz,omitempty"`
+}
+
+type GetBooksResponse struct {
+	api.Response
+	Data []struct {
+		Asks [][]string `json:"asks"`
+		Bids [][]string `json:"bids"`
+		Ts   int64      `json:"ts,string"`
+	} `json:"data"`
+}
