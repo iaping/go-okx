13290699f596cdab3f5aa674fcb53055868d784b
aping <czp010443@aliyun.com>
2022-11-21 14:21:01 +0800

    /api/v5/users/subaccount/list

---
+++ rest/api/subaccount/get_list.go
@@ -0,0 +1,34 @@
+package subaccount
+
+import "github.com/iaping/go-okx/rest/api"
+
+func NewGetList(param *GetListParam) (api.IRequest, api.IResponse) {
+	return &api.Request{
+		Path:   "/api/v5/users/subaccount/list",
+		Method: api.MethodGet,
+		Param:  param,
+	}, &GetListResponse{}
+}
+
+type GetListParam struct {
+	Enable  bool   `url:"enable,omitempty"`
+	SubAcct string `url:"subAcct,omitempty"`
+	After   int64  `url:"after,omitempty"`
+	Before  int64  `url:"before,omitempty"`
+	Limit   int    `url:"limit,omitempty"`
+}
+
+type GetListResponse struct {
+	api.Response
+	Data []List `json:"data"`
+}
+
+type List struct {
+	Type    string `json:"type"`
+	Enable  bool   `json:"enable"`
+	SubAcct string `json:"subAcct"`
+	Label   string `json:"label"`
+	Mobile  string `json:"mobile"`
+	GAuth   bool   `json:"gAuth"`
+	Ts      int64  `json:"ts,string"`
+}
