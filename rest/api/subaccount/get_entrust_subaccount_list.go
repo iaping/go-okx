77f3c5e209611965c2d61a3c7c7955d56c677ff8
aping <czp010443@aliyun.com>
2022-11-21 14:38:50 +0800

    /api/v5/users/entrust-subaccount-list

---
+++ rest/api/subaccount/get_entrust_subaccount_list.go
@@ -0,0 +1,24 @@
+package subaccount
+
+import "github.com/iaping/go-okx/rest/api"
+
+func NewGetEntrustSubaccountList(param *GetEntrustSubaccountListParam) (api.IRequest, api.IResponse) {
+	return &api.Request{
+		Path:   "/api/v5/users/entrust-subaccount-list",
+		Method: api.MethodGet,
+		Param:  param,
+	}, &GetEntrustSubaccountListResponse{}
+}
+
+type GetEntrustSubaccountListParam struct {
+	SubAcct string `url:"subAcct,omitempty"`
+}
+
+type GetEntrustSubaccountListResponse struct {
+	api.Response
+	Data []EntrustSubaccountList `json:"data"`
+}
+
+type EntrustSubaccountList struct {
+	SubAcct string `json:"subAcct"`
+}
