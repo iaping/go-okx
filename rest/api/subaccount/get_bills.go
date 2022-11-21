19a902b335f876d7c5a4dc6a8bab38bdc1e22ed7
aping <czp010443@aliyun.com>
2022-11-21 14:34:32 +0800

    /api/v5/asset/subaccount/bills

---
+++ rest/api/subaccount/get_bills.go
@@ -0,0 +1,34 @@
+package subaccount
+
+import "github.com/iaping/go-okx/rest/api"
+
+func NewGetBills(param *GetBillsParam) (api.IRequest, api.IResponse) {
+	return &api.Request{
+		Path:   "/api/v5/asset/subaccount/bills",
+		Method: api.MethodGet,
+		Param:  param,
+	}, &GetBillsResponse{}
+}
+
+type GetBillsParam struct {
+	Ccy     string `url:"ccy,omitempty"`
+	Type    string `url:"type,omitempty"`
+	SubAcct string `url:"subAcct,omitempty"`
+	After   int64  `url:"after,omitempty"`
+	Before  int64  `url:"before,omitempty"`
+	Limit   int    `url:"limit,omitempty"`
+}
+
+type GetBillsResponse struct {
+	api.Response
+	Data []Bills `json:"data"`
+}
+
+type Bills struct {
+	BillId  string `json:"billId"`
+	Ccy     string `json:"ccy"`
+	Amt     string `json:"amt"`
+	Type    string `json:"type"`
+	SubAcct string `json:"subAcct"`
+	Ts      int64  `json:"ts,string"`
+}
