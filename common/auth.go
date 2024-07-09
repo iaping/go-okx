18bf3d59a781ff4d7658b030d77da0ad6e48efeb
jmz <1125378902@qq.com>
2024-07-09 16:56:10 +0800

    add access objectID

--- common/auth.go
+++ common/auth.go
@@ -1,14 +1,16 @@
 package common
 
 type Auth struct {
+	ObjectID   string
 	ApiKey     string
 	SecretKey  string
 	Passphrase string
 	Simulated  bool
 }
 
-func NewAuth(apiKey, secretKey, passphrase string, simulated bool) Auth {
+func NewAuth(objectID, apiKey, secretKey, passphrase string, simulated bool) Auth {
 	return Auth{
+		ObjectID:   objectID,
 		ApiKey:     apiKey,
 		SecretKey:  secretKey,
 		Passphrase: passphrase,
